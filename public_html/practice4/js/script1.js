0f8ec5070a6d2f1826ed69338e73fc380b99996c
38068 <38068@DESKTOP-SRPFKQ2>
2020-09-28 16:33:22 +0300

    5 module solution added

--- public_html/practice4/js/script1.js
+++ public_html/practice4/js/script1.js
@@ -3,23 +3,75 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-function a(){   
- 
- return "Ok!";
+function a() {
+
+    return "Ok!";
 }
 //Scope
 
+var x = 5;
+
+function a(x) {
+    
+    x= x || 10;
+    
+   // var x = 2;
+
+    x+=3;
+
+    return x;
+
+}
+
+function b() {
+        return x;
+    }
+
+//x=10;
+//console.log(x);
+
 //String concat
 
+var string = "String", string2=" 2";
+
+string+=string2;
+string=string+string2;
+
 // Math (undefined)
+x=5+6;
+
+x=(6+9)/3;
+
+
+
+
 
 //Equality + if
+var x=0,y=false;
+
+//x= x==y;
+//x= x===y;
 
 //True False
 
+if (true && 1 && "Any text" && -1 && "false"){
+    x=true;
+};
+
+if (false || 0 || "" || NaN || undefine || null){
+    x=true;
+} else {
+    
+  x=11;  
+}
+
 //{
 
+
+
 // for loop
+
+
 
 // default values handling
 
