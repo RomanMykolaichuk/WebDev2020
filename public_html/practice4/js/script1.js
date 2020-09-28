32c2b6197bb0bcb27ad107cad467c50b520498b1
38068 <38068@DESKTOP-SRPFKQ2>
2020-09-28 12:56:09 +0300

    5 module solution added

---
+++ public_html/practice4/js/script1.js
@@ -0,0 +1,42 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function a(){
+  
+   
+ 
+ return "Ok!";
+}
+
+//Scope
+
+//String concat
+
+// Math (undefined)
+
+//Equality + if
+
+//True False
+
+//{
+
+// for loop
+
+// default values handling
+
+
+//object creation ways
+
+//arrays creation ways, length
+
+
+//foreach
+
+//fake namespaces
+
+//IIFE
+
+
+
